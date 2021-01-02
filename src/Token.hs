563ea51371f8fa7365a9f76c93baa57354a45e8a
Neimhin <nrobinso@tcd.ie>
2021-01-02 14:18:16 +0000

    pagination

--- src/Token.hs
+++ src/Token.hs
@@ -1,5 +1,5 @@
 module Token where
 import Data.ByteString.UTF8 (fromString)
 
-token = fromString "1fe2a228d6bbd64035d29f6aecc01a363a826b62"
+token = fromString "35c34cd6218e8fded33e74406ac12f7744bee4af"
 authenticationName = fromString "Neimhin"
