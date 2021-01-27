5bc28de64ca40489517b8a6b3c871239e542975d
Neimhin <nrobinso@tcd.ie>
2021-01-27 20:50:24 +0000

    trying pull concurrent

--- app/Main.hs
+++ app/Main.hs
@@ -2,10 +2,19 @@ module Main where
 
 import Lib
 import System.Environment
+import Data.Aeson
+import Control.Concurrent
 
 main :: IO ()
 main = do
-  progname <- getProgName
-  (startingUsername:_) <- getArgs
-  putStrLn $ "Running Neimhin's program: " ++ progname
-  neimhin'sFunc startingUsername
+  threadIds <- mapM forkIO (map getChainAndSaveToFile ["neimhin","fabpot", "andrew", "taylorotwell", "egoist", "HugoGiraudel", "ornicar", "bebraw","nelsonic"])
+  return ()
+  --let links = (foldl (++) [] linkss)
+  --let nodes = generateNodes links
+  --encodeFile ("alltogether.json" :: FilePath) (links, nodes)
+
+getChainAndSaveToFile :: String -> IO ()
+getChainAndSaveToFile startingUsername = do
+  chain <- getChainOfHighestContributors startingUsername
+  let links = generateLinks chain
+  encodeFile ("json/"++(startingUsername ++ ".json") :: FilePath) links
