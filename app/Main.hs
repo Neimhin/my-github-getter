9a7dc145e37ed0056a8a3926ecf05f902a77bdbf
Neimhin <nrobinso@tcd.ie>
2021-01-27 20:42:22 +0000

    buildable concurrent. untested

--- app/Main.hs
+++ app/Main.hs
@@ -9,14 +9,14 @@ import Control.Concurrent
 
 main :: IO ()
 main = do
-  linkss <- mapM getChainAndSaveToFile ["neimhin","fabpot", "andrew", "taylorotwell", "egoist", "HugoGiraudel", "ornicar", "bebraw","nelsonic"]
-  let links = (foldl (++) [] linkss)
-  let nodes = generateNodes links
-  encodeFile ("alltogether.json" :: FilePath) (links, nodes)
+  threadIds <- mapM forkIO (map getChainAndSaveToFile ["neimhin","fabpot", "andrew", "taylorotwell", "egoist", "HugoGiraudel", "ornicar", "bebraw","nelsonic"])
+  return ()
+  --let links = (foldl (++) [] linkss)
+  --let nodes = generateNodes links
+  --encodeFile ("alltogether.json" :: FilePath) (links, nodes)
 
-getChainAndSaveToFile :: String -> IO [Link]
+getChainAndSaveToFile :: String -> IO ()
 getChainAndSaveToFile startingUsername = do
   chain <- getChainOfHighestContributors startingUsername
   let links = generateLinks chain
   encodeFile ("json/"++(startingUsername ++ ".json") :: FilePath) links
-  return links
